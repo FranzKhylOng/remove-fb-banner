f1d8c179f971a8f39891a98f59cd119e25c9b761
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2025-07-14 23:45:57 +0800

    remove fb home feed

--- Content.js
+++ Content.js
@@ -1,10 +1,23 @@
 (function() {
     'use strict';
 
-    if (window.location.hostname === "www.facebook.com" && window.location.pathname.startsWith("/messages")) {
-        const bannerDiv = document.querySelector('div[role="banner"]');
-        if (bannerDiv) {
-            bannerDiv.remove();
+    if (window.location.hostname === "www.facebook.com") {
+        if (window.location.pathname.startsWith("/messages")) {
+            const bannerDiv = document.querySelector('div[role="banner"]');
+            if (bannerDiv) {
+                bannerDiv.remove();
+            }
+        } else {
+            document.documentElement.innerHTML = '';
+            document.documentElement.style.background = 'black';
+            const messageDiv = document.createElement('div');
+            messageDiv.textContent = `Franz, I beg you, please don't.`;
+            messageDiv.style.color = 'white';
+            messageDiv.style.position = 'absolute';
+            messageDiv.style.top = '50%';
+            messageDiv.style.left = '50%';
+            messageDiv.style.transform = 'translate(-50%, -50%)';
+            document.body.appendChild(messageDiv);
         }
     }
-})();
+})()    ;
