8b7c83a3f457f0653ce9feb3f6caf577d63dd954
franzkhylong <franzkhylm.ong@gmail.com>
2025-09-16 19:58:42 +0800

    include instagram

--- Content.js
+++ Content.js
@@ -1,23 +1,37 @@
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    if (window.location.hostname === "www.facebook.com") {
-        if (window.location.pathname.startsWith("/messages")) {
-            const bannerDiv = document.querySelector('div[role="banner"]');
-            if (bannerDiv) {
-                bannerDiv.remove();
-            }
-        } else {
-            document.documentElement.innerHTML = '';
-            document.documentElement.style.background = 'black';
-            const messageDiv = document.createElement('div');
-            messageDiv.textContent = `Franz, I beg you, please don't.`;
-            messageDiv.style.color = 'white';
-            messageDiv.style.position = 'absolute';
-            messageDiv.style.top = '50%';
-            messageDiv.style.left = '50%';
-            messageDiv.style.transform = 'translate(-50%, -50%)';
-            document.body.appendChild(messageDiv);
-        }
+  if (window.location.hostname === "www.facebook.com") {
+    if (window.location.pathname.startsWith("/messages")) {
+      const bannerDiv = document.querySelector('div[role="banner"]');
+      if (bannerDiv) {
+        bannerDiv.remove();
+      }
+    } else {
+      document.documentElement.innerHTML = "";
+      document.documentElement.style.background = "black";
+      const messageDiv = document.createElement("div");
+      messageDiv.textContent = `Franz, I beg you, please don't.`;
+      messageDiv.style.color = "white";
+      messageDiv.style.position = "absolute";
+      messageDiv.style.top = "50%";
+      messageDiv.style.left = "50%";
+      messageDiv.style.transform = "translate(-50%, -50%)";
+      document.body.appendChild(messageDiv);
     }
-})()    ;
+  }
+  if (window.location.hostname === "www.instagram.com") {
+    if (!window.location.pathname.startsWith("/its_khyl/saved/")) {
+      document.documentElement.innerHTML = "";
+      document.documentElement.style.background = "black";
+      const messageDiv = document.createElement("div");
+      messageDiv.textContent = `Franz, I beg you, please don't.`;
+      messageDiv.style.color = "white";
+      messageDiv.style.position = "absolute";
+      messageDiv.style.top = "50%";
+      messageDiv.style.left = "50%";
+      messageDiv.style.transform = "translate(-50%, -50%)";
+      document.body.appendChild(messageDiv);
+    }
+  }
+})();
